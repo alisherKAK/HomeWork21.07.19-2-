85d70a8e63f7568079a7e983629fb7777dae5998
alisherKak <alisher.alish333@gmail.com>
2019-07-23 00:23:31 +0600

    Home Work 21.07.19

--- HomeWork21_07_19(2)/Program.cs
+++ HomeWork21_07_19(2)/Program.cs
@@ -1,15 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork21_07_19_2_
 {
+    public interface IProduct
+    {
+        string Description();
+    }
+
+    public interface IProcessor : IProduct
+    { }
+
+    public interface IMainboard : IProduct
+    {
+        string ShowProcessor(IProcessor processor);
+    }
+
+    public interface IAbstractFactory
+    {
+        IMainboard GetMainboard();
+        IProcessor GetProcessor();
+    }
+
+    public class Dell : IAbstractFactory
+    {
+        public IMainboard GetMainboard()
+        {
+            return new DellMainboard();
+        }
+
+        public IProcessor GetProcessor()
+        {
+            return new DellProcessor();
+        }
+    }
+
+    public class Sony : IAbstractFactory
+    {
+        public IMainboard GetMainboard()
+        {
+            return new SonyMainboard();
+        }
+
+        public IProcessor GetProcessor()
+        {
+            return new SonyProcessor();
+        }
+    }
+
+    public class DellMainboard : IMainboard
+    {
+        public string Description()
+        {
+            return "This is Dell's mainboard";
+        }
+
+        public string ShowProcessor(IProcessor processor)
+        {
+            return $"In Dell mainboard({processor.Description()})";
+        }
+    }
+
+    public class DellProcessor : IProcessor
+
+    {
+        public string Description()
+        {
+            return "This is Dell's processor";
+        }
+    }
+
+    public class SonyMainboard : IMainboard
+    {
+
+        public string Description()
+        {
+            return "This is Sony's mainboard";
+        }
+
+        public string ShowProcessor(IProcessor processor)
+        {
+            return $"In Sony mainboard({processor.Description()})";
+        }
+    }
+
+    public class SonyProcessor : IProcessor
+    {
+        public string Description()
+        {
+            return "This is Sony's processor";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            ClientCode(new Dell());
+            ClientCode(new Sony());
+        }
+
+        static void ClientCode(IAbstractFactory factory)
+        {
+            IMainboard mainboard = factory.GetMainboard();
+            IProcessor processor = factory.GetProcessor();
+
+            Console.WriteLine(mainboard.Description());
+            Console.WriteLine(mainboard.ShowProcessor(processor));
         }
     }
 }
---
+++ Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private Singleton(){}
+        private static Singleton _instance;
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+
+        public static void SomeLogin()
+        {
+            Console.WriteLine("Hello! I'm Singleton and I'm only one in this program");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if(s1 == s2)
+            {
+                Console.WriteLine("Singleton works correctly");
+            }
+            else
+            {
+                Console.WriteLine("Singleton works incorrectly");
+            }
+        }
+    }
+}
